650276cb2e0edffd924058a675f7824b072542a7
Juliana Santos <juhpkz2017@gmail.com>
2024-08-20 23:55:55 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+let totalGeral;
+limpar();
+
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    //calcular o preço, o subtotal
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos');
+    //adicionar no carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
+        <section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span>${nomeProduto}<span class="texto-azul">${preco}</span>
+        </section>
+      </section>`
+    //atualizar o valor
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
+}
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+}
